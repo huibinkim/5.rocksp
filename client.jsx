676bceb0b1abe302365004c3e8bb2183196f034b
huibinkim <94955669+huibinkim@users.noreply.github.com>
2023-05-22 12:09:48 +0900

    changes

--- client.jsx
+++ client.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDom from "react-dom/client";
 
-import RSP from "./RSPhooks";
+import RSP from "./RSP";
 ReactDom.createRoot(document.querySelector("#root")).render(<RSP />);
 // const React = require("react");
 // const ReactDom = require("react-dom/client");
